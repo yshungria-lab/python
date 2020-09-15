27057a50059f657926567d73e86dc386207c9885
Yojan Stiben Hungría <69788548+yshungria-lab@users.noreply.github.com>
2020-09-14 22:54:34 -0500

    Add files via upload

---
+++ docstrings.py
@@ -0,0 +1,12 @@
+def suma(a, b):
+    """Suma dos valores a y b.
+
+    param int a cualquier número entero
+    param int b cualquier número entero
+    reuturns la sumatoria de a y b
+    """
+    total = a + b
+    return total
+
+
+print(suma(2, 4))
